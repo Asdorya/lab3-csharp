d20e8c76b5ba83841d2142b0a6de930ec50636a0
Никешина <dashulichka2020@gmail.com>
2026-03-16 12:03:58 +0300

    Update

--- Lab3.Auth/Program.cs
+++ Lab3.Auth/Program.cs
@@ -1,13 +1,34 @@
-// Lab3.Auth/Program.cs
-using Lab3.Auth.Services;
-
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-builder.Services.AddSingleton<IAuthService, AuthService>();
+
+// Add services to the container.
 
 var app = builder.Build();
-app.MapControllers();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+});
+
 app.Run();
 
-// Частичный класс нужен для доступа из тестов
-public partial class Program { }
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
